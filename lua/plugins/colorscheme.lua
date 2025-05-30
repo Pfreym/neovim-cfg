6b176139f6691d70e3effb828962ee11b27db2f7
Simon <spcompsys@gmail.com>
2025-05-30 22:25:10 +1000

    re-synced

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "arcticicestudio/nord-vim" },
+  { "folke/tokyonight.nvim" },
+  { "Mofiqul/dracula.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
--- lua/plugins/rusttools.lua
+++ lua/plugins/rusttools.lua
@@ -2,6 +2,16 @@
     {
         'neovim/nvim-lspconfig',
         'simrat39/rust-tools.nvim',
-    },
+        "williamboman/mason.nvim",
+            opts = {
+            ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+ 
+       }, 
+       },
+       }, 
 }
 
